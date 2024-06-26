74325d65d23c3efca30c0e8d156a2f97fb94e1a7
Kacper <07Potion@gmail.com>
2024-06-26 22:30:27 +0200

    0.4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, pygame, random, os
+import sys, pygame, random, os, math
 pygame.init()
 
 def load_image(name, scale):
@@ -16,18 +16,18 @@ class Bird(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image("bird.png", 0.35)
-        self.rect.x, self.rect.y = (screen.get_width() / 2) - (self.image.get_size()[0] / 2), (screen.get_height() / 2) - (self.image.get_size()[1] / 2)
+        self.rect.x, self.rect.y = (width / 2) - (height / 2), (screen.get_height() / 2) - (self.image.get_size()[1] / 2)
         self.area = self.rect
 
     def up(self):
         self.rect.y += -300 * dt
 
-class Cloud(pygame.sprite.Sprite):
+class Pipe(pygame.sprite.Sprite):
 
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image("cloud.png", random.uniform(0.1, 0.2))
-        self.rect.topleft = 1000, random.uniform(0, 100)
+        self.image, self.rect = load_image("pipe.png", 0.4)
+        self.rect.x, self.rect.y = -width, random.uniform(550, 320)
+
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 print(main_dir)
@@ -35,29 +35,46 @@ data_dir = os.path.join(main_dir, "assets")
 
 size = width, height = 1000, 600
 bird_speed = [0, 2]
-cloud_speed = (-1, 0)
 
+pygame.display.set_caption("PyBird")
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-cloud = Cloud()
+bg = pygame.image.load("assets/background.png")
+
+bg = pygame.transform.scale(bg, (1100, 600))
+scroll_x = 0
+bg_x = 0
+
+pipe = Pipe()
+
+pipes = pygame.sprite.Group()
+pipes.add(pipe)
 
 bird = Bird()
 
+pygame.display.set_icon(bird.image)
+
 while running:
     screen.blit(bird.image, bird.rect)
-    screen.blit(cloud.image, cloud.rect)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    cloud.rect = cloud.rect.move(cloud_speed)
+    scroll_x -= 1
+    bg_x -= 1
 
-    print(cloud.rect.x, cloud.rect.y)
+    screen.blit(bg, (scroll_x, 0))
+    screen.blit(bg, (bg_x + width, 0))
 
-    screen.fill("blue")
+    if scroll_x <= -width:
+        scroll_x = width
+
+    if bg_x <= -width:
+        bg_x = width
+
 
     bird.rect = bird.rect.move(bird_speed)
     keys = pygame.key.get_pressed()
@@ -71,8 +88,9 @@ while running:
 
 
     screen.blit(bird.image, bird.rect)
-    screen.blit(cloud.image, cloud.rect)
+
     pygame.display.flip()
+    pygame.display.update()
 
     dt = clock.tick(60) / 1000
 
