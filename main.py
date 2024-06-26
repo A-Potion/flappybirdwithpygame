412c21d32cde78ef775bb0011c4a962835fac3a1
Kacper <07Potion@gmail.com>
2024-06-26 21:05:10 +0200

    0.3

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ def load_image(name, scale):
 
     return image, image.get_rect()
 
+class Bird(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image("bird.png", 0.35)
+        self.rect.x, self.rect.y = (screen.get_width() / 2) - (self.image.get_size()[0] / 2), (screen.get_height() / 2) - (self.image.get_size()[1] / 2)
+        self.area = self.rect
+
+    def up(self):
+        self.rect.y += -300 * dt
+
+class Cloud(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image("cloud.png", random.uniform(0.1, 0.2))
+        self.rect.topleft = 1000, random.uniform(0, 100)
+
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 print(main_dir)
 data_dir = os.path.join(main_dir, "assets")
@@ -24,36 +42,36 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-cloud = pygame.image.load("assets/cloud.png")
+cloud = Cloud()
 
-bird, birdpos = load_image("bird.png", 0.35)
-bird_width = bird.get_size()[0]
-bird_height = bird.get_size()[1]
-
-birdpos.x, birdpos.y, = (screen.get_width() / 2) - (bird_width / 2), (screen.get_height() / 2) - (bird_height / 2)
+bird = Bird()
 
 while running:
-    screen.blit(bird, birdpos)
+    screen.blit(bird.image, bird.rect)
+    screen.blit(cloud.image, cloud.rect)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    print(birdpos.y)
+    cloud.rect = cloud.rect.move(cloud_speed)
+
+    print(cloud.rect.x, cloud.rect.y)
 
     screen.fill("blue")
 
-    birdpos = birdpos.move(bird_speed)
+    bird.rect = bird.rect.move(bird_speed)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        if birdpos.top <= 0:
-            birdpos.y += -2
+        if bird.rect.top <= 0:
+            bird.rect.y += -2
         else:
-            birdpos.y += -300 * dt
-    elif birdpos.bottom >= height:
-        birdpos.y += -2
+            bird.up()
+    elif bird.rect.bottom >= height:
+        bird.rect.y += -2
 
 
-    screen.blit(bird, birdpos)
+    screen.blit(bird.image, bird.rect)
+    screen.blit(cloud.image, cloud.rect)
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
