6d3a394d98b2406b496a9ee840750ed8626323d4
Kacper <07Potion@gmail.com>
2024-06-26 19:26:27 +0200

    0.2

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
-import sys, pygame
+import sys, pygame, random, os
 pygame.init()
 
+def load_image(name, scale):
+    fullpath = os.path.join(data_dir, name)
+    image = pygame.image.load(fullpath)
+
+    size = image.get_size()
+    size = size[0] * scale, size[1] * scale
+    image = pygame.transform.scale(image, size)
+
+    return image, image.get_rect()
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+print(main_dir)
+data_dir = os.path.join(main_dir, "assets")
+
 size = width, height = 1000, 600
-speed = [0, 2]
+bird_speed = [0, 2]
+cloud_speed = (-1, 0)
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-bird = pygame.image.load("bird.png")
-bird = pygame.transform.scale(bird, (200, 200))
-birdpos = bird.get_rect()
 
-bdf = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+cloud = pygame.image.load("assets/cloud.png")
+
+bird, birdpos = load_image("bird.png", 0.35)
+bird_width = bird.get_size()[0]
+bird_height = bird.get_size()[1]
+
+birdpos.x, birdpos.y, = (screen.get_width() / 2) - (bird_width / 2), (screen.get_height() / 2) - (bird_height / 2)
 
 while running:
-    screen.blit(bird, (bird_pos))
+    screen.blit(bird, birdpos)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    print(birdpos.y)
+
     screen.fill("blue")
 
-    birdrect = birdrect.move(speed)
+    birdpos = birdpos.move(bird_speed)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        birdpos.y += -300 * dt
+        if birdpos.top <= 0:
+            birdpos.y += -2
+        else:
+            birdpos.y += -300 * dt
+    elif birdpos.bottom >= height:
+        birdpos.y += -2
+
 
     screen.blit(bird, birdpos)
     pygame.display.flip()
